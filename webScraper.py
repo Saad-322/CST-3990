530afbdb4ff6a35a7f7f4bbdfd42b778653c270f
Saad-322 <saady6nov@gmail.com>
2021-11-20 22:51:15 +0000

    scraped data into text file. crawl delay. made into function

--- webScraper.py
+++ webScraper.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
-page = 0
-content = requests.get("https://www.gradcracker.com/search/computing-technology/graduate-jobs-in-london-and-south-east").text
-while page <= 1:
+import time
+
+def find_jobs(p):
+    page = p
+    if p == 0:
+        content = requests.get("https://www.gradcracker.com/search/computing-technology/graduate-jobs-in-london-and-south-east").text
+    else:
+        content = requests.get("https://www.gradcracker.com/search/computing-technology/graduate-jobs-in-london-and-south-east?page="+str(page)).text       
     soup = BeautifulSoup(content,"lxml")
     jobs = soup.find_all("div",class_="job-item")
     for job in jobs: 
         try:
-            name = job.find("h2").a
-            url = name['href']
+            h2_tag = job.find("h2").a
+            name = h2_tag.text.strip()
+            url = h2_tag['href'].strip()
             subtitle = job.find("h2").div
             info = job.find_all("ul",class_="item fa-ul")
             information_S_L = info[0].find_all("li")
@@ -18,17 +24,24 @@ while page <= 1:
             grades = information_G_D[0].text.strip()
             deadline = information_G_D[-1].text.strip()
 
-            print("Name: ",name.text.strip())
-            print("Location: ",location)
-            print("Deadline: ",deadline)
-            print("Salary: ",salary)
-            print("Grades: ",grades)
-            # print("Subtitle: ",subtitle.text.strip().replace('  •  ',''))
-            print("URL: ",url.strip())
-            print()
+            with open(f"jobs.txt","a") as f:
+                f.write(f"Name: {name} \n")
+                f.write(f"Location: {location} \n")
+                f.write(f"Deadline: {deadline} \n")
+                f.write(f"Salary: {salary} \n")
+                f.write(f"Grades: {grades} \n")
+                # f.write("Subtitle: ",subtitle.text.strip().replace('  •  ',''))
+                f.write(f"URL: {url} \n")
+                f.write(f"\n")
+            print("File Saved")
         except:
             pass
-    page = page + 1 
-    content = requests.get("https://www.gradcracker.com/search/computing-technology/graduate-jobs-in-london-and-south-east?page="+str(page)).text       
-    
+if __name__ == '__main__':
+    p = 0
+    while(p<=6):
+        find_jobs(p)
+        seconds_wait = 1
+        print(f"waiting {seconds_wait} seconds")
+        time.sleep(seconds_wait)
+        p = p + 1
    
