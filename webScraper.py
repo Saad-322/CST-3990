bdf55e11707edb858c94d79baf491706da3d981f
Saad-322 <saady6nov@gmail.com>
2021-11-20 19:41:59 +0000

    gradcracker scraped

--- webScraper.py
+++ webScraper.py
@@ -1,9 +1,34 @@
 from bs4 import BeautifulSoup
-with open("home.html","r") as home_html_file:
-    content = home_html_file.read()
+import requests
+page = 0
+content = requests.get("https://www.gradcracker.com/search/computing-technology/graduate-jobs-in-london-and-south-east").text
+while page <= 1:
     soup = BeautifulSoup(content,"lxml")
-    courses = soup.find_all("div",class_="card")
-    for course in courses:
-        print(course.h5.text.split()[0])
-        print(course.p.text)
-        print(course.a.text.split()[-1])
+    jobs = soup.find_all("div",class_="job-item")
+    for job in jobs: 
+        try:
+            name = job.find("h2").a
+            url = name['href']
+            subtitle = job.find("h2").div
+            info = job.find_all("ul",class_="item fa-ul")
+            information_S_L = info[0].find_all("li")
+            information_G_D = info[-1].find_all("li")
+            salary = information_S_L[0].text.strip()
+            location = information_S_L[-1].text.strip()
+            grades = information_G_D[0].text.strip()
+            deadline = information_G_D[-1].text.strip()
+
+            print("Name: ",name.text.strip())
+            print("Location: ",location)
+            print("Deadline: ",deadline)
+            print("Salary: ",salary)
+            print("Grades: ",grades)
+            # print("Subtitle: ",subtitle.text.strip().replace('  •  ',''))
+            print("URL: ",url.strip())
+            print()
+        except:
+            pass
+    page = page + 1 
+    content = requests.get("https://www.gradcracker.com/search/computing-technology/graduate-jobs-in-london-and-south-east?page="+str(page)).text       
+    
+   
